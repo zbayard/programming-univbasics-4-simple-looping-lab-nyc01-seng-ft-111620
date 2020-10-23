d20cf76b9469d86e0b59d422a3c4962d7f535570
Zach Bayard <zbayard@gmail.com>
2020-10-23 13:56:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -13,3 +13,11 @@ def loop_message_n_times(message , num)
     count += 1
   end
 end
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
