6b9ec6f3a4aea549920de6a0e958a4bb4a14d0f5
Zach Bayard <zbayard@gmail.com>
2020-10-23 13:54:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,9 @@
 def loop_message_five_times(string)
-  puts string
-  puts string
-  puts string
-  puts string
-  puts string
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+    end
 end
 
 def loop_message_n_times(message , num)
